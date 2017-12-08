c292aeeec3925a9c56456f5384f0da288d9d7d9b
Andrei Fokau <andrei@5monkeys.se>
2017-12-08 13:01:56 +0100

    Use Markdown 2.4 for Python 2.6.

--- cio/plugins/md.py
+++ cio/plugins/md.py
@@ -1,9 +1,5 @@
 from __future__ import unicode_literals
-
-import logging
-
 from .txt import TextPlugin
-from .. import PY26
 
 
 class MarkdownPlugin(TextPlugin):
@@ -11,11 +7,7 @@ class MarkdownPlugin(TextPlugin):
     ext = 'md'
 
     def render(self, data):
-        if PY26:
-            logging.warning('Markdown is not supported for Python 2.6')
-            return data
-        else:
-            import markdown
-            if data:
-                extensions = self.settings.get('EXTENSIONS', [])
-                return markdown.markdown(data, extensions=extensions)
+        import markdown
+        if data:
+            extensions = self.settings.get('EXTENSIONS', [])
+            return markdown.markdown(data, extensions=extensions)
